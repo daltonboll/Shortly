85a7111ba2ea6f226b422e2b3144f797adcf778a
Dalton Boll <daltonboll@gmail.com>
2015-12-10 16:56:28 -0800

    Style home page

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id          :integer          not null, primary key
+#  destination :string
+#  shortened   :string
+#  favorited   :integer          default([]), is an Array
+#  description :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Link < ActiveRecord::Base
 end
